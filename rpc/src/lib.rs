39201aec6f8e3e095df05734ab67795faf701b6a
gjl_glb <aaaglb@aaa.com>
2023-07-05 23:39:35 +0800

    add rpc_health & update lib

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -6,7 +6,7 @@ pub mod parsed_token_accounts;
 // pub mod evm_rpc_impl;
 pub mod rpc;
 pub mod rpc_completed_slots_service;
-// pub mod rpc_health;
+pub mod rpc_health;
 // pub mod rpc_pubsub;
 // pub mod rpc_pubsub_service;
 // pub mod rpc_service;
