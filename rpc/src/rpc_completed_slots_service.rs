f8f8907281350ecd9baffc4737e3fc8cceac77b2
gjl_glb <aaaglb@aaa.com>
2023-07-05 23:38:25 +0800

    add rpc_completed_slots_service & update lib

---
+++ rpc/src/rpc_completed_slots_service.rs
@@ -0,0 +1,32 @@
+use {
+    crate::rpc_subscriptions::RpcSubscriptions,
+    client::rpc_response::SlotUpdate,
+    ledger::blockstore::CompletedSlotsReceiver,
+    sdk::timing::timestamp,
+    std::{
+        sync::Arc,
+        thread::{Builder, JoinHandle},
+    },
+};
+
+pub struct RpcCompletedSlotsService;
+impl RpcCompletedSlotsService {
+    pub fn spawn(
+        completed_slots_receiver: CompletedSlotsReceiver,
+        rpc_subscriptions: Arc<RpcSubscriptions>,
+    ) -> JoinHandle<()> {
+        Builder::new()
+            .name("solana-rpc-completed-slots-service".to_string())
+            .spawn(move || {
+                for slots in completed_slots_receiver.iter() {
+                    for slot in slots {
+                        rpc_subscriptions.notify_slot_update(SlotUpdate::Completed {
+                            slot,
+                            timestamp: timestamp(),
+                        });
+                    }
+                }
+            })
+            .unwrap()
+    }
+}
