e488a6f696376cb52a792fab01d1c5c6d20252e7
965389850 <965389850@qq.com>
2023-10-13 10:41:48 +0800

    fix: `storage-proto v1.9.29 `: `cargo::rerun-if-changed=proto/confirmed_block.proto`

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
+echo Installing rust-lang...
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 rustup component add rustfmt
 
 rustup update
+echo Changing the version of rust to 1.73
+rustup default 1.73
 
 type=`uname  -a`
 if [[ $type =~ "Darwin" ]];then
+    echo Install apple dependance
     softwareupdate --install-rosetta
 else
+    echo Install linux dependence
     sudo apt-get update
     sudo apt-get install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang make cmake protobuf-compiler
 fi
 
 
-git clone https://github.com/mikewang68/sino-chain.git
-cd sino-chain
+# git clone https://github.com/mikewang68/sino-chain.git
 
 FILE_PATH=~/.local/share/sino
 
@@ -26,34 +30,34 @@ else
 fi
 
 
-evm_bridge=sino-chain/target/debug/evm-bridge
-faucet=sino-chain/target/debug/sino-faucet
-gossip=sino-chain/target/debug/sino-gossip
-sino_genesis=sino-chain/target/debug/sino-genesis
-sino_keygen=sino-chain/target/debug/sino-keygen
-test_validator=sino-chain/target/debug/test-validator
-validator=sino-chain/target/debug/sino-validator
+evm_bridge=target/debug/evm-bridge
+faucet=target/debug/sino-faucet
+gossip=target/debug/sino-gossip
+sino_genesis=target/debug/sino-genesis
+sino_keygen=target/debug/sino-keygen
+test_validator=target/debug/test-validator
+validator=target/debug/sino-validator
 
 if [ -f "$evm_bridge" -a -f "$faucet" -a -f "$gossip" -a -f "$sino_genesis" -a -f "$sino_keygen" -a -f "$test_validator" -a -f "$validator" ]; then
     echo "Build has been completed"
 else
-    cd sino-chain
-    cargo build --release
+    cargo build
 fi
 
-# cd target/debug
-cd target/release
-
+cd target/debug
 echo "copying files to $FILE_PATH"
 cp sino evm-bridge sino-faucet sino-gossip sino-genesis sino-keygen test-validator sino-validator $FILE_PATH
 
 cd ~
 zsh=.zshrc
 bash=.bashrc
+path="export PATH=\"~/.local/share/sino:\$PATH\""
 if [ -f "$zsh" ]; then
-    echo "export PATH=~/.local/share/sino:\$PATH\" >> .zshrc
+    echo writting $path to .zshrc...
+    echo "$path" >> .zshrc
 fi
 
 if [ -f "$bash" ]; then
-    echo "export PATH=~/.local/share/sino:\$PATH\" >> .bashrc
+    echo writting $path to .bashrc...
+    echo "$path" >> .bashrc
 fi
