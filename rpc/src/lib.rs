43e4695f0335e90a9da8848cdae4e1b033730311
gjl_glb <aaaglb@aaa.com>
2023-11-02 10:31:12 +0800

    去掉第6行注释

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -3,7 +3,7 @@ mod cluster_tpu_info;
 pub mod max_slots;
 pub mod optimistically_confirmed_bank_tracker;
 pub mod parsed_token_accounts;
-// pub mod evm_rpc_impl;
+pub mod evm_rpc_impl;
 pub mod rpc;
 pub mod rpc_completed_slots_service;
 pub mod rpc_health;
